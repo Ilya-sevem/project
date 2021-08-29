8a1053e933befaf8395d875302855fc60e2bd418
Sevem <ilya.nikolodov@gmail.com>
2021-08-29 12:45:17 +0500

    first practice exercise

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 "use strict";
 
-const user = "Ivan";
-alert(`Durak ${user}`);
+prompt("Сколько фильмов вы уже смотрели?","6");
+const numberOfFilms = prompt("Сколько фильмов вы уже смотрели?","6");
 
-console.log(2 * 2 + 2 != 6);
+const personalMuvieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false
+};
 
-const isSome = 1,
-	isSomeother = 2;
+const a = prompt("Один из последних просмотренных фильмов?", "Logan"),
+	b = prompt('На сколько оцените его?','8.1'),
+	c = prompt("Один из последних просмотренных фильмов?", "Logan"),
+	d = prompt('На сколько оцените его?','8.1');
+
+personalMuvieDB.movies[a] = b;
+personalMuvieDB.movies[c] = d;
+
+console.log(personalMuvieDB);
